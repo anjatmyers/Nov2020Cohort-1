ac457dc80279d22b6408dbf9ca7a22931ce28f53
Veronica Lino <veronica@digitalcrafts.com>
2021-01-07 15:18:07 -0600

    artspeakers

--- week08/4-Thursday/veros-notes-dont-touch/artspeakers/routes/speakers.js
+++ week08/4-Thursday/veros-notes-dont-touch/artspeakers/routes/speakers.js
@@ -1,10 +1,40 @@
 const express = require("express");
 const router = express.Router();
+let dataFile = require('../data/data.json');
 
-
+let pageSpeakers = dataFile.speakers; //[{}, {}, {}]
 router.get('/speakers', (req, res) => {
+    //all images
+    let pagePhotos = [];
+
+    pageSpeakers.forEach(speakerObj =>{
+        pagePhotos = pagePhotos.concat(speakerObj.artwork);
+    })
+
+    res.render('speakers', {
+        artwork: pagePhotos,
+        speakers: pageSpeakers
+    })
+})
+
+router.get('/speakers/:speakerid', (req, res) => {
   
-    res.render('speakers')
+    //
+    let photos = [];
+    let speakers = [];
+
+    pageSpeakers.forEach(speakerObj => {
+
+        if(speakerObj.shortname === req.params.speakerid){
+            photos = speakerObj.artwork;
+            speakers.push(speakerObj)
+        }
+    })
+
+    res.render('speakers', {
+        artwork: photos,
+        speakers: speakers
+    })
 })
 
 module.exports = router;
