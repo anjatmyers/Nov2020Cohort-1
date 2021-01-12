f7c35bebf32226280cb095448a55d62a822cd403
Veronica Lino <veronica@digitalcrafts.com>
2021-01-12 16:01:56 -0600

    chat artspeakers finished version

--- week08/4-Thursday/veros-notes-dont-touch/artspeakers/app.js
+++ week08/4-Thursday/veros-notes-dont-touch/artspeakers/app.js
@@ -1,6 +1,9 @@
 const express = require("express");
 
 const app = express();
+//const io = require('socket.io')();
+const socket = require('socket.io');
+
 
 //ejs
 app.set("view engine", "ejs");
@@ -13,7 +16,27 @@ app.use(require("./routes/index"));
 app.use(require("./routes/speakers"));
 app.use(require("./routes/feedback"));
 app.use(require("./routes/api"));
+app.use(require("./routes/chat"));
 
-app.listen(3000, () => {
+let server = app.listen(3000, () => {
   console.log(`listening on port 3000`);
 });
+//io.attach(server)
+let io = socket(server);
+
+io.on('connection', (socket)=>{
+
+  console.log('you are connect');
+  
+  //listening for messages from client
+  socket.on('postMessage', (msg)=>{
+
+    console.log(msg);
+    //broadcast to all connected servers
+    io.emit('updateMessages', msg)
+  })
+})
+
+
+
+
