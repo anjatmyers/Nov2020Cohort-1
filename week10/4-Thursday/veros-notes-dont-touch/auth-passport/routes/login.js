d596fab1a42690771f803cc208256aa7bd78b28e
Veronica Lino <veronica@digitalcrafts.com>
2021-01-21 14:26:45 -0600

    auth

--- week10/4-Thursday/veros-notes-dont-touch/auth-passport/routes/login.js
+++ week10/4-Thursday/veros-notes-dont-touch/auth-passport/routes/login.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/login", (req, res) => {
-  res.send("home page");
+  res.render('login')
 });
 
 module.exports = router;
--- week10/4-Thursday/veros-notes-dont-touch/auth-passport/routes/registration.js
+++ week10/4-Thursday/veros-notes-dont-touch/auth-passport/routes/registration.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/registration", (req, res) => {
-  res.send("home page");
+  res.render('registration')
 });
 
 module.exports = router;
