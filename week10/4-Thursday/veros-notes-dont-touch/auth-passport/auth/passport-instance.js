b8ae7d838967deaafd61fce1c66d61f66d47a2b2
Veronica Lino <veronica@digitalcrafts.com>
2021-01-21 15:50:51 -0600

    passport

---
+++ week10/4-Thursday/veros-notes-dont-touch/auth-passport/auth/passport-instance.js
@@ -0,0 +1,5 @@
+const passport = require("passport");
+
+Object.freeze(passport);
+
+module.exports = passport;
--- week10/4-Thursday/veros-notes-dont-touch/auth-passport/routes/login.js
+++ week10/4-Thursday/veros-notes-dont-touch/auth-passport/routes/login.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const router = express.Router();
 
-router.get("/login", (req, res) => {
+
+router.get("/login",  (req, res) => {
   res.render('login')
 });
+
+
+router.post('/login', )
 
 module.exports = router;
