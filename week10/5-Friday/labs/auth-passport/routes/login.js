4b2a37a6888a28f4ff13816f0d4af7377ed573a7
Veronica Lino <veronica@digitalcrafts.com>
2021-01-22 09:05:32 -0600

    auth-passport

---
+++ week10/5-Friday/labs/auth-passport/routes/login.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const router = express.Router();
+const passport = require('passport');
+
+
+router.get("/login",  (req, res) => {
+  res.render('login')
+});
+
+
+router.post('/login', passport.authenticate('local', {successRedirect: '/protected', failureRedirect: '/login'}))
+
+module.exports = router;
