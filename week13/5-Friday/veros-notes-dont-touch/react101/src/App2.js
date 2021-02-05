8b40e117711bb294417df04f1ab21a11487653bb
Veronica Lino <veronica@digitalcrafts.com>
2021-02-05 14:04:57 -0600

    styling react components

--- week13/5-Friday/veros-notes-dont-touch/react101/src/App2.js
+++ week13/5-Friday/veros-notes-dont-touch/react101/src/App2.js
@@ -1,10 +1,33 @@
 import React, { Component } from "react";
 import {Container, Row, Col} from 'react-bootstrap';
 import CardList from './components/CardList';
+import  './assets/styles/global.css';
+import Card from './components/cards/Card';
+import data from './data/cardContacts';
+
 
 class App2 extends Component {
+
+
   render() {
+
+    let contactsArr = data.map(contact =>{
+
+        return <Card name={contact.name}  img={contact.imgURL} 
+        tel={contact.phone} email={contact.email}/>
+    })
+
     return <>
+
+        <div>
+            <h1 className="heading">My Contacts</h1>
+            {contactsArr}
+        </div>
+        {/* <div className="bg-purple">
+            Hello world
+        </div>
+
+        <div style={{color: 'purple', fontSize:'40px', textAlign: 'center'}}>Hello world</div>
 
         <Container fluid>
 
@@ -14,9 +37,16 @@ class App2 extends Component {
                
             </Row>
 
-        </Container>
+        </Container> */}
     </>;
   }
+}
+
+const styles={
+    purple: {
+        color: 'orange',
+        fontSize: '40px'
+    }
 }
 
 export default App2;
--- week13/5-Friday/veros-notes-dont-touch/react101/src/index.js
+++ week13/5-Friday/veros-notes-dont-touch/react101/src/index.js
@@ -2,8 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import App2 from './App2';
-import 'bootstrap/dist/css/bootstrap.min.css';
-
+// import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
   <React.StrictMode>
